ebc15f1e041c6dc9a4df3c8205bd20a831102890
carlosprivitera <carlosprivitera@yahoo.com.ar>
2023-04-24 13:34:29 -0300

    mm

---
+++ ProyectoMavenHolaMundo/demo/src/main/java/Servlet2.java
@@ -0,0 +1,38 @@
+
+
+package main.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "Servlet2", urlPatterns = { "/servlet2" })
+public class Servlet2 extends HttpServlet {
+    private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String var0 = "";
+        String var1 = "";
+        try {
+            var0 = request.getParameter("param0");
+            var1 = request.getParameter("param1");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        response.setContentType(CONTENT_TYPE);
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Servlet2</title></head>");
+        out.println("<body>");
+        out.println("<p>The servlet has received a GET. This is the reply.</p>");
+        out.println("</body></html>");
+        out.close();
+    }
+}
