04efe16f67cd787b1676d80f41d3466529109c69
carlosprivitera <carlosprivitera@yahoo.com.ar>
2023-05-23 15:57:34 -0300

    23-may 15:57

--- ProyectoMavenHolaMundo2/demo/src/main/java/CalcularArea.java
+++ ProyectoMavenHolaMundo2/demo/src/main/java/CalcularArea.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -8,9 +9,17 @@ import javax.servlet.http.*;
 @WebServlet(name = "CalcularArea", urlPatterns = { "/calculararea" })
 public class CalcularArea extends HttpServlet {
     private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
+
+    
+
     public void init(ServletConfig config) throws ServletException {
+
         super.init(config);
+
     }
+
+     
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String var0 = "";
         String var1 = "";
@@ -24,18 +33,22 @@ public class CalcularArea extends HttpServlet {
         }
         Float b = Float.parseFloat(var0);
         Float a = Float.parseFloat(var1);
+
         response.setContentType(CONTENT_TYPE);
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<head><title>Servlet2</title></head>");
         out.println("<body>");
+
         out.println("Área = " + calcularArea(b, a, var2));
+
         out.println("<p>The servlet has received a GET. This is the reply.</p>");
         out.println("</body></html>");
         out.close();
     }
     private Float calcularArea(Float b, Float a, String q) {
        Float resultado = 0.0f; 
+       }
        if(q.equals("CalcularRec")) {
             resultado = b * a;
        }else{
@@ -43,4 +56,8 @@ public class CalcularArea extends HttpServlet {
        }
        return resultado;
     }
+
+    
+   
+
 }
